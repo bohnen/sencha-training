2028b3ef88ddeef47b40cba2d921f73d166610e9
bohnen <bohnen.net@gmail.com>
2014-09-16 23:51:48 +0900

    memo

--- MyApp/app.js
+++ MyApp/app.js
@@ -14,38 +14,51 @@ Ext.application({
     name: 'Sencha',
 
     launch: function() {
+        Ext.create('Ext.data.TreeStore',{
+            storeId: 'TreeStore',
+            fields:[
+                'title','link','author','contentSnippet','content',
+                {name: 'leaf', defaultValue: true}
+            ],
+
+            root:{
+                leaf: false
+            },
+
+            proxy: {
+                type: 'jsonp',
+                url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                reader:{
+                    type: 'json',
+                    rootProperty: 'responseData.feed.entries'
+                }
+            }
+        });
+
         Ext.create("Ext.tab.Panel", {
             fullscreen: true,
             tabBarPosition: 'bottom',
 
             items:[
                 {
+                    title: 'Home',
+                    iconCls: 'home',
+                    cls: 'home',
+                    html: [
+                        '<img width="65%" src="http://staging.sencha.com/img/sencha.png" />',
+                        '<h1>Welcome to Sencha Touch</h1>',
+                        "<p>We're creating the Getting Started app, which demonstrates how ",
+                        "to use tabs, lists, and forms to create a simple app.</p>",
+                        '<h2>Sencha Touch</h2>'
+                    ].join("")
+                },
+                {
                     xtype: 'nestedlist',
                     title: 'Blog',
                     iconCls: 'star',
                     displayField: 'title',
 
-                    store: {
-                        type: 'tree',
-
-                        fields:[
-                            'title','link','author','contentSnippet','content',
-                            {name: 'leaf', defaultValue: true}
-                        ],
-
-                        root:{
-                            leaf: false
-                        },
-
-                        proxy: {
-                            type: 'jsonp',
-                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
-                            reader:{
-                                type: 'json',
-                                rootProperty: 'responseData.feed.entries'
-                            }
-                        }
-                    },
+                    store: 'TreeStore',
 
                     detailCard: {
                         xtype: 'panel',
@@ -58,8 +71,47 @@ Ext.application({
                             this.getDetailCard().setHtml(post.get('content'));
                         }
                     }
+                },
+                {
+                    title: 'Contact',
+                    iconCls: 'user',
+                    xtype: 'formpanel',
+                    url: 'contact.php',
+                    layout: 'vbox',
+                    items:[
+                        {
+                            xtype: 'fieldset',
+                            title: 'Contact us',
+                            instructions: '(email address is optional)',
+                            height: 285,
+                            items:[
+
+                                {
+                                    xtype: 'textfield',
+                                    label: 'Name'
+                                },
+                                {
+                                    xtype: 'emailfield',
+                                    label: 'Email'
+                                },
+                                {
+                                    xtype: 'textareafield',
+                                    label: 'Message'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Send',
+                            ui: 'Confirm',
+                            handler: function(){
+                                this.up('formpanel').submit();
+                            }
+                        }
+                    ]
+
                 }
             ]
-        })
+        });
     }
 });
