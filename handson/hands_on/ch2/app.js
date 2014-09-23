2d1c13c74f21f6d2298e9b7539e365e6cfef4c3c
bohnen <bohnen.net@gmail.com>
2014-09-24 07:11:05 +0900

    change contents

--- handson/hands_on/ch2/app.js
+++ handson/hands_on/ch2/app.js
@@ -1,17 +1,25 @@
 /**
  * Created by bohnen on 2014/09/23.
  */
-Ext.onReady(function(){
-    var data = {
-        name: 'Taxi Amsterdam',
-        description: 'The only taxi in Amsterdam that does not circle around.'
-    };
+var data = {
+    name: 'Taxi Amsterdam',
+    description: 'The only taxi in Amsterdam that does not circle around.'
+};
 
-    Ext.create('Ext.Component', {
+var c = null;
+
+Ext.require('Ext.Component');
+
+Ext.onReady(function(){
+    c = Ext.create('Ext.Component', {
        tpl: '<h1>{name}</h1><p>{description}</p>',
        data: data,
        styleHtmlContent: true,
        cls: 'box',
        renderTo: Ext.getBody()
     });
+    
+    data.description = "We like tourists a lot!";
+
+    c.setData(data);
 });
