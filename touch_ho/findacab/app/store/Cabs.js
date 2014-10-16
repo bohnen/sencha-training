04b87b37d722861789da9b06299c5cc2d25d406a
bohnen <bohnen.net@gmail.com>
2014-10-16 23:52:50 +0900

    implement jsonp

--- touch_ho/findacab/app/store/Cabs.js
+++ touch_ho/findacab/app/store/Cabs.js
@@ -1,12 +1,18 @@
 Ext.define('FindACab.store.Cabs', {
     extend: 'Ext.data.Store',
-    requires: ['Ext.data.proxy.Ajax'],
+    requires: ['Ext.data.proxy.JsonP'],
     config: {
         model: 'FindACab.model.Cab',
         autoLoad: true,
         proxy: {
-            type: "ajax",
-            url:  "data/data.json",
+            type: 'jsonp',
+            noCache: false,
+            url: Utils.Commons.YELP_API,
+            extraParams:{
+                term: Utils.Commons.YELP_TERM,
+                ywsid: Utils.Commons.YELP_KEY,
+                location: Utils.Commons.LOCATION
+            },
             reader: {
                 type: "json",
                 rootProperty: "businesses"
