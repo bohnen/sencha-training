f78ec0de750577d17ac1883aea832430b0dac62f
bohnen <bohnen.net@gmail.com>
2014-10-23 23:48:10 +0900

    half road to view

--- touch_ho/findacab/app/view/Main.js
+++ touch_ho/findacab/app/view/Main.js
@@ -1,7 +1,18 @@
 Ext.define('FindACab.view.Main',{
     extend: 'Ext.Container',
-    xtype: 'mainview',
+    requires: [
+        'FindACab.view.Overview',
+        'FindACab.view.DetailView'
+    ],
     config: {
-        html: 'here comes the view.'
+        layout: 'hbox',
+        items:[{
+            xtype: 'overview',
+            flex: 1,
+            store: 'Cabs'
+        }, {
+            xtype: 'detailview',
+            flex: 3
+        }]
     }
 });
